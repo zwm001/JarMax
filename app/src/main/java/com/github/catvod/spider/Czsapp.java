821a4deb8741ed165d5c03624a4b7fd2b5a97d3d
zwm001 <110072013+zwm001@users.noreply.github.com>
2022-10-17 11:46:54 +0800

    Update Czsapp.java

--- app/src/main/java/com/github/catvod/spider/Czsapp.java
+++ app/src/main/java/com/github/catvod/spider/Czsapp.java
@@ -124,25 +124,7 @@ public class Czsapp extends Spider {
         return keyword;
     }
 
-
-
-    private static String convertUnicodeToCh(String str) {
-        Pattern pattern = Pattern.compile("(\\\\u(\\w{4}))");
-        Matcher matcher = pattern.matcher(str);
-
-        // 迭代，将str中的所有unicode转换为正常字符
-        while (matcher.find()) {
-            String unicodeFull = matcher.group(1); // 匹配出的每个字的unicode，比如\u67e5
-            String unicodeNum = matcher.group(2); // 匹配出每个字的数字，比如\u67e5，会匹配出67e5
-
-            // 将匹配出的数字按照16进制转换为10进制，转换为char类型，就是对应的正常字符了
-            char singleChar = (char) Integer.parseInt(unicodeNum, 16);
-
-            // 替换原始字符串中的unicode码
-            str = str.replace(unicodeFull, singleChar + "");
-        }
-        return str;
-    }
+    
     private String mmd5(String str) {
         try {
             MessageDigest messageDigest = MessageDigest.getInstance("MD5");
@@ -391,9 +373,14 @@ public class Czsapp extends Spider {
         String str4;
         Elements G8;
         try {
-            String K2 = OkHttpUtil.string("https://czspp.com/v_play/" + str2 + ".html", Headers());
-            Document UR = Jsoup.parse(K2);
-            Matcher matcher = Y.matcher(K2);
+            String html = OkHttpUtil.string(siteUrl + "/v_play/" + str2 + ".html", Headers());
+            html = jumpbtwaf(siteUrl,html);
+           
+           Document UR = Jsoup.parse(html);
+//            String K2 = OkHttpUtil.string("https://czspp.com/v_play/" + str2 + ".html", Headers());
+//            Document UR = Jsoup.parse(K2);
+ //           Matcher matcher = Y.matcher(K2);
+            Matcher matcher = Y.matcher(html);
             if (matcher.find()) {
                 String group = matcher.group(1);
                 String KEY = matcher.group(2);
